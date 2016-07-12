c262468f20dfb74c7c333a040d16fb16f08b1e48
ebirukov <john_soft@mail.ru>
2016-07-12 10:33:42 +0400

    Update sync-runner.sh

--- scripts/sync-runner.sh
+++ scripts/sync-runner.sh
@@ -5,6 +5,6 @@ chmod -R 0400 ~/.ssh/
 ssh-keyscan -p 722 $GIT_HOST >> ~/.ssh/known_hosts
 cd ${GIT_BASE_PATH}
 chmod +x /sync-repo.sh
-crontab -l | { cat; echo "*/5	*	*	*	*	/sync-repo.sh > /var/log/sync.log 2>&1"; } | crontab -
+crontab -l | { cat; echo "*/15	*	*	*	*	/sync-repo.sh > /var/log/sync.log 2>&1"; } | crontab -
 touch /var/log/sync.log
 crond -d 0 && tail -f /var/log/sync.log
